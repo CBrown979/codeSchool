62ad4b167b55428cf19a5eddd5939d664891434b
Candice Bianca Brown <candicebrown97@gmail.com>
2017-09-05 10:37:57 +0000

    readable chunks from Streams

--- codeSchoolExercises.js
+++ codeSchoolExercises.js
@@ -1,4 +1,14 @@
 // Chapter 3 - Streams
 var fs = require('fs');
-var file= fs.createReadStream("fruits.txt");
-//
+
+var file = fs.createReadStream('fruits.txt');
+
+file.on('readable', function(){
+  var chunk;
+  while(null !== (chunk = file.read())){
+    console.log(chunk.toString());
+  }
+});
+// Inside the callback, read the data chunks from the stream and print them to the 
+// console using console.log() - you might want to use a while loop to do this. 
+// Don't forget to call toString() on the data before printing it.
