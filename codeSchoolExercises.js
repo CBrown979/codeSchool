72c3c4474d71901b3c9e58678d3a47f05f8d51e5
Candice Bianca Brown <candicebrown97@gmail.com>
2017-09-05 12:16:00 +0000

    Piping to read from stream & write directly to process.stdout

--- codeSchoolExercises.js
+++ codeSchoolExercises.js
@@ -12,3 +12,9 @@ file.on('readable', function(){
 // Inside the callback, read the data chunks from the stream and print them to the 
 // console using console.log() - you might want to use a while loop to do this. 
 // Don't forget to call toString() on the data before printing it.
+
+//File Piping
+//Instead of manually listening for the 'readable' event on the Readable stream, let's use pipe to read from the stream and write directly to process.stdout.
+var fs = require('fs');
+var file = fs.createReadStream('fruits.txt');
+file.pipe(process.stdout);
