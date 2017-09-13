fc88e42809ef5e2f74200fbfd06498a1f3fef288
Candice Bianca Brown <candicebrown97@gmail.com>
2017-09-13 22:31:09 +0000

    completed level5 of RealTimeWeb

--- codeSchoolExercises.js
+++ codeSchoolExercises.js
@@ -1,103 +1,129 @@
-// Chapter 4 - Modules
+// Chapter 5- Express
+//Express Routes
+//Let's create an express route that accepts GET requests on '/tweets' and responds by sending back a static HTML file.
+//Create a GET route for '/tweets' and give it the proper callback. The callback function should accept two arguments: the request and the response.
+var express = require('express');
+var app = express();
+app.get('/tweets', function(response, request){});
 
-//Notice the two different files: high_five.js on the left side and app.js on the right. 
-//The code as it's written will not work, high_five.js isn't exporting anything.
-//Add the proper exports line to have a successful high five!
-var highfive = function() {
-  console.log("smack!!");
+//Send back the file tweets.html, which lives under the project's root path. Remember to use __dirname to locate tweets.html.
+var express = require('express');
+var app = express();
+app.get('/tweets', function(request, response){
+  response.sendFile(__dirname + "/tweets.html");
+});
+//Finally, have the express app listen on port 8080.
+app.listen(8080);
+
+//Route Params
+//Let's create a route that accepts dynamic arguments in the URL path and responds with the quote from the proper author.
+
+var express = require('express');
+var app = express();
+
+var quotes = {
+  'einstein': 'Life is like riding a bicycle. To keep your balance you must keep moving',
+  'berners-lee': 'The Web does not just connect machines, it connects people',
+  'crockford': 'The good thing about reinventing the wheel is that you can get a round one',
+  'hofstadter': 'Which statement seems more true: (1) I have a brain. (2) I am a brain.'
 };
-module.exports = highfive;
+//Start by creating a GET route for '/quotes' that takes a name parameter as part of the URL path.
+app.get('/quotes/:name', function(request, response){
+ //Now, use the name parameter from the URL to retrieve a quote from the quotes object and write it out to the response. Note: No piping here, just write the quote string to the response like 
+  //you did in previous levels (and then close the response).
+  var name= request.params.name;
+  response.send(quotes[name]);
+});
 
+app.listen(8080);
 
-//Notice the app.js file with the myRequest function below. Let's refactor myRequest out to its own my_request.js module.
-//Move the myRequest function and the http require into my_request.js
-app.js
+//Instead of just writing out the quote to the response, let's try using an EJS template to render the response.
+//Next, make the name and the quote data available to the template.
 
-var myRequest = require('./my_request');
+var express = require('express');
+var app = express();
 
-myRequest('Hello, this is dog.');
-
-my_request.js
-
-var http = require('http');
-
-var myRequest = function(message) {
-  var request = http.request('http://codeschool.com', function(response) {
-    response.pipe(process.stdout, { end: false });
+var quotes = {
+  'einstein': 'Life is like riding a bicycle. To keep your balance you must keep moving',
+  'berners-lee': 'The Web does not just connect machines, it connects people',
+  'crockford': 'The good thing about reinventing the wheel is that you can get a round one',
+  'hofstadter': 'Which statement seems more true: (1) I have a brain. (2) I am a brain.'
+};
+app.get('/quotes/:name', function(req, res) {
+  var quote = quotes[req.params.name];
+  res.render('quote.ejs', {
+    name: req.params.name,
+    quote: quote
   });
+});
+app.listen(8080);
 
-  request.write(message);
-  request.end();
-};
-module.exports = myRequest;
+//Inside quote.ejs, add the code needed to render the data you passed to the template.
+<h2>Quote by <%= name %></h2>
+<blockquote>
+  <%= quote %>
+</blockquote>
 
-
-//Require the my_request.js module in app.js.
-//Here's one possible answer.
-my_request.js
-var http = require('http');
-var myRequest = function(message) {
-  var request = http.request('http://codeschool.com', function(response) {
-    response.pipe(process.stdout, { end: false });
-  });
-
-  request.write(message);
-  request.end();
-};
-module.exports = myRequest;
-
-app.js
-var myRequest = require('./my_request');
-myRequest('Hello, this is dog.');
-
-
-//below code in logger.js file
-var warn = function(message) {
-  console.log("Warning: " + message);
-};
-
-var info = function(message) {
-  console.log("Info: " + message);
-};
-
-var error = function(message) {
-  console.log("Error: " + message);
-};
-  
-//In the logger.js file, export the info function so we can use it in app.js by assigning it to the exports object.
-exports.info = info;
-
-//In the logger.js file, export the warn function so we can use it in app.js by assigning it to the exports object.
-exports.warn = warn;
-
-//In the logger.js file, export the error function so we can use it in app.js by assigning it to the exports object.
-exports.error = error;
-
-
-//Dependency
-//Add two dependencies to our package.json file, connect and underscore. We'll want to use version 2.1.1 of connect and version 1.3.3 of underscore.
-//Add the connect dependency to package.json
-//Add the underscore dependency to package.json
-{
-  "name": "My Awesome Node App",
-  "version": "1",
-  "dependencies": {
-    "connect": "2.1.1",
-      "underscore": "1.3.3"    
-  }
+//URL Building
+// Let's create a page which calls the Twitter search API and displays the last few results for Code School. 
+// The first step is to construct the proper URL, which is all you need to do in this challenge.
+// Complete the URL options which will be sent into the the url module's format method. 
+// The URL you'll want to construct is the following: http://search.twitter.com/search.json?q=codeschool
+var url = require('url');
+options = {
+  // add URL options here
+  protocol: "http://search.twitter.com/search.json?q=codeschool",
+  host: "search.twitter.com",
+  pathname: "search.json",
+  query: {
+  q: "codeschool"
 }
+};
+var searchURL = url.format(options);
+console.log(searchURL);
 
-//Semantic Versioning
-//We want to make sure we are always up-to-date with the most recent patch-level changes to our dependencies when we run npm install.
-//Update the connect version on package.json to fetch the latest patch-level changes. 
-//All we have to do is add one character to the beginning of the version number. -- you have to add the tilda ~
-//and the underscore version on package.json to fetch the latest patch-level changes.
-{
-  "name": "My Awesome Node App",
-  "version": "1",
-  "dependencies": {
-    "connect": "~2.2.1",
-    "underscore": "~1.3.3"
+//Doing the Request
+// Next, we'll use the request module to make a simple web request and log the response to the console. You can use this example in the README.
+var url = require('url');
+
+var options = {
+  protocol: "http:",
+  host: "search.twitter.com",
+  pathname: '/search.json',
+  query: { q: "codeschool"}
+};
+var searchURL = url.format(options);
+//To start, require the request module and assign the return function to a variable.
+var request=require('request');
+//Next, issue a request to searchURL. Remember, the callback function for the request
+//function takes three arguments: error, response and body.
+request(searchURL, function(error, response, body){
+  //Finally, log the response body to the console using console.log().
+  console.log(body);  
+});
+
+
+//Express Server
+var url = require('url');
+var request = require('request');
+//Require the express module.
+var express=require('express');
+
+var options = {
+  protocol: "http:",
+  host: "search.twitter.com",
+  pathname: '/search.json',
+  query: {
+    q: "codeschool"
   }
-}
-
+};
+var searchURL = url.format(options);
+//Create the Express server and name it app.
+var app=express(); // Create server here
+//Create a route for GET requests to / (root path). Remember, the callback function 
+//takes two arguments: a request req and a response res.
+app.get('/', function(req, res){
+  //In our new route, issue a request to searchURL and pipe the results into the response.
+  request(searchURL).pipe(res);
+//  Finally, tell app to listen on port 8080.
+}).listen(8080);
