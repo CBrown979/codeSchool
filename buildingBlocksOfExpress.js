cc3a7ba1bd4fcb98a02d5ccbb2f88591cec8c5ae
Candice Bianca Brown <candicebrown97@gmail.com>
2017-09-16 16:18:53 +0000

    completed Leve 2

--- buildingBlocksOfExpress.js
+++ buildingBlocksOfExpress.js
@@ -1,44 +1,168 @@
-//Building Blocks of Express - Level 1
+//Building Blocks of Express - Level 2
 
-var express = require('express'); // require express module & assign to express variable
-var app = express(); // create an application instance using function assigned to express & assign to app variable
+//Mounting Middleware
+//Given an application instance is set to the app variable, what following function call would you use to mount a middleware 
+//called logger --> app.use(logger) 
 
-app.get('/locations', function(request, response){}); //Using our application instance, app, create a new route that 
-//accepts GET requests on the /locations URL path. Remember to pass a callback function which takes a request and response.
+//Default Middleware
+//The only middleware that's shipped with Express 4 is express-static
 
-app.get('/locations', function(request, response){
-  var cityNames = ['Caspiana', 'Indigo', 'Paradise']; //Respond with an array of city names. The city names should be Caspiana, Indigo and Paradise.
-  response.send(cityNames);
-});
-app.listen(3001); //Bind application to port 3001.
-
-app.listen(3001, function(){
-  console.log("Running Express"); //When our application is ready to receive requests, print the string "Running Express" to the console.
-});
-
-//When we run our previous code and issue a GET request to the /locations endpoint, the Content-Type header for the response be set to application/json
-
-//If we were to craft a response sending a string of text with the response.send() function, just like the following code,
-//Express would set this Content-Type to text/html
-app.get('/locations', function(request, response) {
-    var cities = '<ul><li>Caspiana</li><li>Indigo</li></ul>';
-    response.send(cities);
-});
-
-app.get('/locations', function(request, response){}); //create new route for locations
-
-//Now redirect from /locations to /cities path using the Moved Permanently HTTP status code (free hint for you, the code for that is 301)
-response.redirect(301, '/cities');
-
-//Full Example below:'var express = require('express');
+//Express Static
+//Change the code in app.js to use the express-static middleware instead of the response.sendFile() function.
+//The example was:
+var express = require('express');
 var app = express();
-app.get('/cities', function (request, response) {                                                                                                     
+
+app.get('/', function (request, response) {
+  response.sendFile(__dirname + '/public/index.html');
+});
+app.get('/cities', function(req, res){
+  var cities = ['Lotopia', 'Caspiana', 'Indigo'];
+  res.send(cities);
+});
+app.listen(3001);
+
+//Will now remove our app.get() containing the root '/' route. -- updated example below, after mounting the static middleware and 
+//serving files under the public directory --> app.use(express.static('public'));
+//Results:
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/cities', function(req, res){
+  var cities = ['Lotopia', 'Caspiana', 'Indigo'];
+  res.send(cities);
+});
+app.listen(3001);
+
+//Script Tags
+//Within index.html include jquery.js & client.js using script tags
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <title>Cities</title>
+// </head>
+// <body>
+//   <h1>Cities</h1>
+//   <ul class='city-list'></ul>
+
+  <script src="jquery.js"></script>
+  <script src="client.js"></script> 
+
+// </body>
+// </html>
+
+//from within client.js, complete the code for the $.get function so that it calls the /cities URL path, and then runs the appendToList function
+//The first argument should be the /cities and the second should be appendToList.
+
+// $(function(){
+
+  $.get('/cities', appendToList); 
+
+//   function appendToList(cities) {
+//     var list = [];
+//     for(var i in cities){
+//       list.push($('<li>', { text: cities[i] }));
+//     }
+//     $('.city-list').append(list);
+//   }
+// });
+
+
+//Logging Middleware
+//On the response object, listen to the event that's emitted when the response has been handed off from Express to the underlying Operating System.
+response.on('finish', function(){});
+
+//Inside of the finish callback, calculate the duration of the request by subtracting the startTime from a new Date object. 
+//Store the duration in the duration variable, which has already been declared for you.
+// module.exports = function (request, response, next) {
+//   var startTime = +new Date();
+//   var stream = process.stdout;
+//   var duration = null;
+
+  response.on('finish', function () {
+     duration = +new Date() - startTime;
+  });
+// };
+
+//Using the stream object, which holds a reference to standard out, 
+//write the following message: "This request took ____ ms", where ____ is the duration for the request.
+stream.write("This request took " + duration + " ms");
+
+//If we run the code as is, the request will be stuck in our middleware. Call the function that moves processing to the next middleware in the stack.
+next();
+
+//Full Example below:
+module.exports = function (request, response, next) {
+  var startTime = +new Date(); //the plus sign converts dateObject to milliseconds
+  var stream = process.stdout;
+  var duration = null;
+
+  response.on('finish', function () {
+    duration = +new Date() - startTime;
+    stream.write("This request took " + duration + " ms");
+  });
+  next();
+};
+
+//Add Logging Middleware
+In the following code in app.js, we require our new middleware and assign it to a variable called logger.
+var express = require('express');
+var app = express();
+var logger = require('./logger');
+
+//TODO: mount middleware
+app.use(logger)
+//is the function  we call in order to mount the middleware and add it to the stack
+
+app.listen(3000);
+
+
+//Only GET -- Let's build a middleware that ensures only GET requests are allowed to go through.
+//First, in the only_get.js file, create an anonymous function that uses the middleware signature and assign it to module.exports. 
+//Remember, the Express middleware function signature takes three arguments.
+module.exports = function(request, response, next){};
+
+//Use the request object to check if the HTTP method used is 'GET' and if it is, then call the function that moves processing 
+//to the next middleware in the stack.
+if(request.method === 'GET') {
+    next();
+};
+//If the HTTP method is not 'GET', then complete the request by sending back a message that says 'Method is not allowed'.
+else {
+    response.send('Method is not allowed');
+  };
+
+//Full Example
+module.exports = function(request, response, next){
+  if(request.method === 'GET') {
+    next();
+  }else {
+    response.send('Method is not allowed');
+  }
+};
+
+//Buildings
+var express = require('express');
+var app = express();
+
+app.use(function(request, response, next){
+  if (request.path === "/cities"){
+    next();
+  } else {
+    response.status(404).json("Path requested does not exist");
+  }
+});
+
+app.get('/cities', function(request, response){
   var cities = ['Caspiana', 'Indigo', 'Paradise'];
-  response.send(cities);
+  response.json(cities);
 });
-app.get('/locations', function(request, response){
-  response.redirect(301, '/cities');
-});
-app.listen(3001, function () {
-  console.log("Running Express");
-});'
+
+app.listen(3000);
+
+//When we run our previous code and issue a GET request to the /buildings endpoint, the response will be a 404 response with
+//Path requested does not exist
+
