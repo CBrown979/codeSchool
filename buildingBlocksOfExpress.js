f65139754e4f64fd2117ec4a63c9fcd9a911cc8a
Candice Bianca Brown <candicebrown97@gmail.com>
2017-09-18 20:58:59 +0000

    completed Level 4

--- buildingBlocksOfExpress.js
+++ buildingBlocksOfExpress.js
@@ -129,3 +129,27 @@ function parseCityName(name) {
   var parsedName = name[0].toUpperCase() + name.slice(1).toLowerCase();
   return parsedName;
 }
+
+
+//Delete Route
+//Create a Dynamic Route for deleting cities and handle for cities that are not in our list.
+
+//Create a DELETE route that takes the city name as its first argument, followed by a callback that
+//takes a request and response objects as arguments.
+app.delete('/cities/:name', function(request, response){});
+
+//Use the built-in JavaScript operator delete (see MDN docs) to remove the property for the city passed as an argument. 
+//Don't forget to use the attribute set in app.param() to look the city up.
+delete cities[request.cityName];
+
+//Use sendStatus() to respond back with a status code of 200.
+response.sendStatus(200);
+
+//Add an if block that checks whether the cityName provided from app.param() has a valid entry before attempting to delete it from 
+//the cities object. If a valid city is not found, then respond with a 404 HTTP status code using the sendStatus() function.
+if(cities[request.cityName]){
+        delete cities[request.cityName];
+    response.sendStatus(200);
+    } else {
+        response.sendStatus(404);
+    }
